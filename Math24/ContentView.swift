bac4a90ee393aea9277ca470c956bce9ac5fa4e8
Kyle <ykyle01@gmail.com>
2025-03-22 01:26:29 -0400

    Implemented skip and restyled UI. Working POC for all operations except division.

--- Math24/ContentView.swift
+++ Math24/ContentView.swift
@@ -8,20 +8,49 @@
 import SwiftUI
 import SwiftData
 
-import SwiftUI
+// Colors
+let lightBlue = Color(red: 0.70, green: 0.80, blue: 0.9)
+let darkBlue = Color(red: 0.94, green: 0.97, blue: 1.0)
+let lightOrange = Color(red: 1, green: 0.95, blue: 0.86)
+let darkOrange = Color(red: 1, green: 0.67, blue: 0.36)
+
+// Define target
+let target = 24
 
 struct ContentView: View {
     @StateObject var gameLogic = GameLogic()
     
     // Define the grid layout with 2 columns
     let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    // Define all possible operations
     let operations: [Character] = ["+", "-", "×", "÷"]
     
     var body: some View {
         VStack {
-            Text("Math 24")
-                .font(.title)
-                .padding()
+            HStack {
+                Button(action: {
+                    gameLogic.skip()
+                 }) {
+                     Text("Skip")
+                         .font(.title)
+                         .padding()
+                         .background(Color.orange)
+                         .foregroundColor(.white)
+                         .cornerRadius(10)
+                 }
+                
+                Button(action: {
+                    gameLogic.refresh()
+                 }) {
+                     Text("Refresh")
+                         .font(.title)
+                         .padding()
+                         .background(Color.orange)
+                         .foregroundColor(.white)
+                         .cornerRadius(10)
+                 }
+            }
+            .padding()
             
             VStack {
                 // Grid container (LazyVGrid)
@@ -31,13 +60,17 @@ struct ContentView: View {
                         ForEach(0..<4, id: \.self) { index in
                             // Card view for each item in the grid
                             if let num = gameLogic.numbers[index] {
-                                NumberCardView(cardName: "\(num)", isSelected: index == gameLogic.selectedNumIndex, isVisible: true)
-                                    .onTapGesture {
-                                        // Change selected card
-                                        gameLogic.selectNum(index: index)
-                                    }
+                                if gameLogic.numbersRemaining == 1 {
+                                    NumberCardView(cardName: "\(num)", backgroundColor: gameLogic.numbers[index] == target ? darkOrange : Color.gray)
+                                } else {
+                                    NumberCardView(cardName: "\(num)", backgroundColor: index == gameLogic.selectedNumIndex ? lightBlue : darkBlue)
+                                        .onTapGesture {
+                                            // Change selected card
+                                            gameLogic.selectNum(index: index)
+                                        }
+                                }
                             } else {
-                                NumberCardView(cardName: "", isSelected: false, isVisible: false)
+                                NumberCardView(cardName: "", backgroundColor: Color.white)
                             }
                         }
                     }
@@ -49,36 +82,37 @@ struct ContentView: View {
                 ForEach(operations, id: \.self) { operation in
                     // Card view for each item in the grid
                     OperationCardView(cardName: operation, isSelected: operation == gameLogic.selectedOperation)
-                    .onTapGesture {
-                        // Change selected card
-                        gameLogic.selectOperation(operation: operation)
-                    }
+                        .onTapGesture {
+                            // Change selected card
+                            gameLogic.selectOperation(operation: operation)
+                        }
+                        .disabled(gameLogic.numbersRemaining == 1)
                 }
             }
         }
+        .scrollDisabled(true)
         .padding()
     }
 }
 
 struct NumberCardView: View {
     let cardName: String
-    let isSelected: Bool
-    let isVisible: Bool
+    let backgroundColor: Color
     
     var body: some View {
         ZStack {
             // Card background color
             RoundedRectangle(cornerRadius: 20)
-                .fill(isVisible ? (isSelected ? Color.blue : Color.gray) : Color.white)
+                .fill(backgroundColor)
                 .frame(height: 150)
             
             // Card label
             Text(cardName)
                 .font(.title)
-                .foregroundColor(.white)
+                .foregroundColor(.black)
                 .bold()
         }
-        .animation(.easeInOut(duration: 0.3), value: isSelected)
+        .animation(.easeInOut(duration: 0.3), value: backgroundColor)
     }
 }
 
@@ -90,13 +124,13 @@ struct OperationCardView: View {
         ZStack {
             // Card background color
             RoundedRectangle(cornerRadius: 20)
-                .fill(isSelected ? Color.blue : Color.gray)
+                .fill(isSelected ? darkOrange : lightOrange)
                 .frame(height: 100)
             
             // Card label
             Text(String(cardName))
                 .font(.title)
-                .foregroundColor(.white)
+                .foregroundColor(isSelected ? lightOrange : darkOrange)
                 .bold()
         }
         .animation(.easeInOut(duration: 0.3), value: isSelected)
--- Math24/GameLogic.swift
+++ Math24/GameLogic.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 class GameLogic: ObservableObject {
+    @Published var generatedNumbers: [Int] = []
     @Published var numbers: [Int?] = []
-    @Published var disabled: [Bool] = []
     @Published var selectedNumIndex: Int?
     @Published var secondNumIndex: Int?
     @Published var selectedOperation: Character?
+    @Published var numbersRemaining: Int
     
     init() {
+        numbersRemaining = 4
         generateRandomNumbers()
+        refresh()
     }
 
     func refresh() {
+        numbers = generatedNumbers
         selectedNumIndex = nil
         selectedOperation = nil
+        numbersRemaining = 4
+    }
+    
+    func skip() {
+        generateRandomNumbers()
+        refresh()
     }
     
     func generateRandomNumbers() {
-        numbers = (1...4).map { _ in Int.random(in: 1...9) }
+        generatedNumbers = (1...4).map { _ in Int.random(in: 1...9) }
     }
     
     func selectNum(index: Int) {
-        if selectedOperation != nil && selectedNumIndex != nil {
+        if selectedOperation != nil && selectedNumIndex != nil && index != selectedNumIndex {
             do {
                 try evaluate(secondNumIndex: index)
             } catch OperationError.unimplementedOperation {
@@ -37,12 +47,12 @@ class GameLogic: ObservableObject {
                 print("Invalid operation")
             }
         } else {
-            selectedNumIndex = index
+            selectedNumIndex = index == selectedNumIndex ? nil : index
         }
     }
     
     func selectOperation(operation: Character) {
-        selectedOperation = operation
+        selectedOperation = operation == selectedOperation ? nil : operation
     }
     
     func evaluate(secondNumIndex: Int) throws {
@@ -60,9 +70,18 @@ class GameLogic: ObservableObject {
                     throw OperationError.invalidOperation
             }
             numbers[firstNumIndex] = nil
-            refresh()
+            numbersRemaining -= 1
+            selectedOperation = nil
+            selectedNumIndex = secondNumIndex
+            
+            // If correct...
+            if numbersRemaining == 1 && numbers[secondNumIndex] == 24 {
+                let seconds = 1.0
+                DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+                    self.skip()
+                }
+            }
         }
-        
     }
 }
 
